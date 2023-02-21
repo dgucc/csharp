b2a3b135a6863115cfc42692a8f427bd595eccc4
dgucc <106461038+dgucc@users.noreply.github.com>
2023-02-21 08:40:56 +0100

    Add files via upload

---
+++ Datasource/Program.cs
@@ -0,0 +1,19 @@
+﻿// <Snippet1>
+
+using System.Data.SqlClient;
+namespace ccrek.sandbox {
+
+	class Program {
+		static void Main(string[] args) {
+			using SqlConnection conn = SingletonTestDb.GetInstance();
+
+			Console.WriteLine("ServerVersion: {0}", conn.ServerVersion);
+			Console.WriteLine("DataSource: {0}", conn.DataSource);
+			Console.WriteLine("Database: {0}", conn.Database.ToString());
+			Console.WriteLine("State: {0}", conn.State.ToString());
+
+
+			Console.Read();
+		}
+	}
+}
---
+++ Datasource/SingletonTestDb.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ccrek.sandbox {
+
+	public class SingletonTestDb {
+		private static SqlConnection _instance = null;
+		private string _connectionString = ConfigurationManager.ConnectionStrings["TestDb"].ToString();
+		private static readonly object _syncObject = new object();
+
+		private SingletonTestDb() {
+			SqlConnection conn = new SqlConnection(_connectionString);
+			conn.Open();
+			_instance = conn;
+		}
+
+		public static SqlConnection GetInstance() {
+			if (_instance == null) {
+				lock (_syncObject) {
+					new SingletonTestDb();
+				}
+			}
+			return _instance;
+		}
+	}
+
+}
