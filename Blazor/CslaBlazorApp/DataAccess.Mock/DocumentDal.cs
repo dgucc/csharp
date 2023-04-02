67ba5462ae4c5edd492d4f2fe86a7ca6a2909b55
dgucc <djvu.guchz@gmail.com>
2023-04-02 07:55:02 +0200

    switch back to Mock

---
+++ Blazor/CslaBlazorApp/DataAccess.Mock/DocumentDal.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Mock;
+    
+public class DocumentDal : IDocumentDal {
+
+    private static readonly List<DocumentDTO> _documentTable = new List<DocumentDTO>{
+        new DocumentDTO {
+            Id=1,
+            PublicationId=1,
+            DocumentType=EnumDocumentType.Report,
+            Language=EnumLanguageCode.French,
+            Thumbnail=File.ReadAllBytes(@"/home/user/workspace/github/csharp/Blazor/CslaBlazorApp/TEMP/cover.jpg")
+        },
+        new DocumentDTO {
+            Id=2,
+            PublicationId=1,
+            DocumentType=EnumDocumentType.Report,
+            Language=EnumLanguageCode.Dutch,
+            Thumbnail=File.ReadAllBytes(@"/home/user/workspace/github/csharp/Blazor/CslaBlazorApp/TEMP/cover.jpg")
+        },
+        new DocumentDTO {
+            Id=3,
+            PublicationId=2,
+            DocumentType=EnumDocumentType.Report,
+            Language=EnumLanguageCode.French,
+            Thumbnail=File.ReadAllBytes(@"/home/user/workspace/github/csharp/Blazor/CslaBlazorApp/TEMP/cover.jpg")
+        },
+        new DocumentDTO {
+            Id=7,
+            PublicationId=2,
+            DocumentType=EnumDocumentType.Report,
+            Language=EnumLanguageCode.Dutch,
+            Thumbnail=File.ReadAllBytes(@"/home/user/workspace/github/csharp/Blazor/CslaBlazorApp/TEMP/cover.jpg")
+        },
+        new DocumentDTO {
+            Id=8,
+            PublicationId=3,
+            DocumentType=EnumDocumentType.Report,
+            Language=EnumLanguageCode.Dutch,
+            Thumbnail=File.ReadAllBytes(@"/home/user/workspace/github/csharp/Blazor/CslaBlazorApp/TEMP/cover.jpg")
+        },
+        new DocumentDTO {
+            Id=9,
+            PublicationId=4,
+            DocumentType=EnumDocumentType.Report,
+            Language=EnumLanguageCode.Dutch,
+            Thumbnail=File.ReadAllBytes(@"/home/user/workspace/github/csharp/Blazor/CslaBlazorApp/TEMP/cover.jpg")
+        }
+    };
+
+    public bool Exists(int id){ 
+        var document = _documentTable.Where(p => p.Id == id).FirstOrDefault();
+            return !(document == null);
+    }
+
+	public DocumentDTO Get(int id){
+        Console.WriteLine("[DAL] Get(id:{0})", id);
+			var document = _documentTable.Where(p => p.Id == id).FirstOrDefault();
+            if (document != null) {
+                return document;
+            } else {
+                throw new KeyNotFoundException($"Id {id}");
+            }
+    }
+    
+    public List<DocumentDTO> Get(){
+        return _documentTable.Where(r => true).ToList();
+    }
+
+    public List<DocumentDTO> GetByPublication(int documentId){
+        return new List<DocumentDTO>();
+    }
+
+    public DocumentDTO Insert(DocumentDTO document){
+        return new DocumentDTO();
+    }
+    
+    public DocumentDTO Update(DocumentDTO document){
+        Console.WriteLine("[DAL] Update()");
+            lock (_documentTable) {
+                var old = Get(document.Id);
+                old.PublicationId = document.PublicationId;
+                old.DocumentType = document.DocumentType;
+                old.Language = document.Language;
+                return old;
+            }
+    }
+    
+    public bool Delete(int id){
+        Console.WriteLine("[DAL] Delete(Document id:{0})", id);
+			var document = _documentTable.Where(p => p.Id == id).FirstOrDefault();
+            if (document != null) {
+                lock (_documentTable) {
+                    _documentTable.Remove(document);
+                    return true;
+                }
+            } else {
+                return false;
+            }            
+    }
+
+}
